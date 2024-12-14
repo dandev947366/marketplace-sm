5944bc314893ab369ce73e8d796e058634e32322
Dan Le <dan.dev947366@gmail.com>
2024-12-14 18:34:09 +0200

    Update

--- contracts/RealEstateMarketplace.sol
+++ contracts/RealEstateMarketplace.sol
@@ -36,6 +36,20 @@ contract RealEstateMarketplace is Ownable, ReentrancyGuard {
         uint timestamp
     );
 
+    struct RealEstateOwner{
+        address ownerAddress;
+        string name;
+        string contactDetails;
+        uint[] ownedProperties;
+    }
+
+    struct RealEstateRenter{
+        address renterAddress;
+        string name;
+        string contacDetails;
+        uint[] rentedProperties;
+        uint[] bookingHistory;
+    }
     struct Booking{
         uint id;
         uint aid;
@@ -75,13 +89,15 @@ contract RealEstateMarketplace is Ownable, ReentrancyGuard {
     uint public securityFee;
     uint public taxPercent;
 
-    mapping(uint => RealEstate) private idToRealEstate;
+    mapping(uint => RealEstate) idToRealEstate;
+    mapping(address => RealEstate[]) RealEstatesOf;
     mapping(uint => Booking[]) bookingsOf;
     mapping(uint => Review[]) reviewsOf;
     mapping(uint => bool) realEstateExist;
     mapping(uint => uint[]) bookedDates;
     mapping(uint => mapping(uint => bool)) isDateBooked;
     mapping(address => mapping(uint => bool)) hasBooked;
+
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can change listing price");
@@ -91,5 +107,63 @@ contract RealEstateMarketplace is Ownable, ReentrancyGuard {
         taxPercent = _taxPercent;
         securityFee = _securityFee;
     };
+
+    // CRUD FOR REAL ESTATE
+    function createRealEstate(
+        string memory _name,
+        string memory _description,
+        string memory _location,
+        string memory _images,
+        uint _rooms,
+        uint _price
+    ) public {
+        require(bytes(_name).length > 0, "Name cannot be empty");
+        require(bytes(_description).length > 0, "Description cannot be empty");
+        require(bytes(_location).length > 0, "Location cannot be empty");
+        require(bytes(_images).length > 0, "Images cannot be empty");
+        require(_rooms > 0, "Rooms cannot be empty");
+        require(_price > 0, "Price cannot be empty");
+
+        _totalRealEstate.increment();
+        uint currentCount = _totalRealEstate.current();
+
+        RealEstate memory realEstate;
+        realEstate.id = currentCount;
+        realEstate.name = _name;
+        realEstate.description = _description;
+        realEstate.location = _location;
+        realEstate.images = _images;
+        realEstate.rooms = _rooms;
+        realEstate.price = _price;
+        realEstate.owner = msg.sender;
+        realEstate.booked = false;
+        realEstate.deleted = false;
+        realEstate.timestamp = block.timestamp;
+        realEstateExist[realEstate.id] = true;
+        idToRealEstate[currentCount] = realEstate;  // Store the real estate in the mapping
+    }
+
+    function updateRealEstate(){}
+    function deleteRealEstate(){}
+    function getRealEstate(){}
+    function getAllRealEstates(){}
+
+    // CRUD FOR BOOKING
+    function createBooking(){}
+    function updateBooking(){}
+    function deleteBooking(){}
+    function getBooking(){}
+    function getAllBookings(){}
+
+    // CRUD FOR REVIEW
+    function createReview(){}
+    function updateReview(){}
+    function deleteReview(){}
+    function getReview(){}
+    function getAllReviews(){}
+
+
+
+
 
 }

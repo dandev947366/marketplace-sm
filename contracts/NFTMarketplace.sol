5b293e9b9d29144be1df597028d750a0ea3b56c3
Dan Le <dan.dev947366@gmail.com>
2024-12-14 14:59:55 +0200

    Update

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// INTERNAL IMPORT FOR NFT OPENZEPPELINE
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+    uint256 public listingPrice = 0.0025 ether;
+    address payable owner;
+    mapping(uint256 => MarketItem) private idMarketItem;
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable ower;
+        uint256 price;
+        bool sold;
+    }
+    event idMarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can change listing price");
+        _;
+    }
+
+    constructor() ERC721("NFT Metaverse Token", "MYNFT") {
+        owner = payable(msg.sender);
+    }
+
+    function updateListingPrice(
+        uint256 _listingPrice
+    ) public payable onlyOwner {
+        listingPrice = _listingPrice;
+    }
+
+    function getListingPrice() public view returns(uint256) {
+        return listingPrice;
+    }
+}
